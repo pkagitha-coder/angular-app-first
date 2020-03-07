02049b58eed705ffd53016ca04a74a543fcf352b
Pradeep Kagithalagudem <gallant.pradeep@gmail.com>
2020-03-07 20:43:00 +0530

    First component albums

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumsComponent } from './components/albums/albums.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AlbumsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
